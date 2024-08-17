c85d4c2d5e999a3552fe0dd09523692e6dcaa14b
mihai-bontea <mihaibontea@protonmail.com>
2024-08-18 00:16:31 +0300

    experimenting with retrieval pointers information

--- main.cpp
+++ main.cpp
@@ -32,10 +32,10 @@ void get_file_handles(const std::wstring& directory, std::vector<HANDLE> &all_fi
                 {
                     all_file_handles.push_back(hFile);
                 }
-                 CloseHandle(hFile);
+//                 CloseHandle(hFile);
             }
         }
-    } while (FindNextFileW(hFind, &findFileData) != 0 && all_file_handles.size() < 10000);
+    } while (FindNextFileW(hFind, &findFileData) != 0 && all_file_handles.size() < 1000);
 }
 
 int main() {
@@ -54,5 +54,63 @@ int main() {
     }
 
     std::cout << "We have " << nr_invalid_handles << " invalid filenames out of " <<  all_file_handles.size() << " files!\n";
+
+    auto hFile = all_file_handles[99];
+
+    // Get the volume handle
+//    HANDLE hVolume = CreateFile(_T("\\\\.\\C:"), 0, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+    HANDLE hVolume = CreateFile(_T("\\\\.\\C:"), GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+
+    if (hVolume == INVALID_HANDLE_VALUE)
+    {
+        _tprintf(_T("Error opening volume, error code %d\n"), GetLastError());
+        CloseHandle(hFile);
+        return 1;
+    }
+
+    // Get the file size
+    LARGE_INTEGER fileSize;
+    GetFileSizeEx(hFile, &fileSize);
+
+    // Allocate memory for the retrieval pointers
+//    DWORD bufferSize = sizeof(STARTING_VCN_INPUT_BUFFER) + sizeof(RETRIEVAL_POINTERS_BUFFER);
+//    LPVOID buffer = malloc(bufferSize);
+    DWORD clusterSize = 4096;
+    DWORD bufferSize = sizeof(RETRIEVAL_POINTERS_BUFFER) + sizeof(LARGE_INTEGER) * (fileSize.QuadPart / clusterSize + 1);
+    LPVOID buffer = malloc(bufferSize);
+
+
+    if (!buffer)
+    {
+        _tprintf(_T("Memory allocation error\n"));
+        CloseHandle(hFile);
+        CloseHandle(hVolume);
+        return 1;
+    }
+
+    // Initialize the input buffer
+    STARTING_VCN_INPUT_BUFFER inputBuffer;
+    inputBuffer.StartingVcn.QuadPart = 0;
+
+    DWORD bytesReturned;
+//    BOOL success = DeviceIoControl(hVolume, FSCTL_GET_RETRIEVAL_POINTERS, &inputBuffer, sizeof(inputBuffer), buffer, bufferSize, &bytesReturned, NULL);
+    BOOL success = DeviceIoControl(hFile, FSCTL_GET_RETRIEVAL_POINTERS, &inputBuffer, sizeof(inputBuffer), buffer, bufferSize, &bytesReturned, NULL);
+
+
+    // Issues here
+    if (!success)
+    {
+        _tprintf(_T("Error retrieving allocation information, error code %d\n"), GetLastError());
+        free(buffer);
+        CloseHandle(hFile);
+        CloseHandle(hVolume);
+        return 1;
+    }
+
+    // Process the retrieval pointers information in the buffer
+    RETRIEVAL_POINTERS_BUFFER* retrievalBuffer = (RETRIEVAL_POINTERS_BUFFER*)buffer;
+
+    // TODO: Process the retrieval pointers information as needed
+
     return 0;
 }
